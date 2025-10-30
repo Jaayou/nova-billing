39b0f98e7bd6dba2e07730ddcd8071e7baafe99d
Jaayou <song1209@gmail.com>
2025-10-30 18:55:44 +0900

    수정

--- src/test/java/com/nova/billing/batch/RegularBillingJobConfigTest.java
+++ src/test/java/com/nova/billing/batch/RegularBillingJobConfigTest.java
@@ -16,9 +16,11 @@ import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBatchTest
 @SpringBootTest
+@Sql(scripts = "classpath:org/springframework/batch/core/schema-h2.sql")
 class RegularBillingJobConfigTest {
 
     @Autowired
